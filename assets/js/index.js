3f5fda717c87eccc3085bdcf889a51dc19f92a40
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-03-21 01:12:49 -0600

    se añaden hrfe a navbar y arryas base

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+const propiedades_alquiler = [
+    {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    }
+    ]
+    
+
+
+    const propiedades_ventas = [
+        {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+        }
+        ]
