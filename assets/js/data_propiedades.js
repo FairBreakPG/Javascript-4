96c6f2f85b963ed27f9770ca382b4cfffbf65bf2
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-03-24 00:40:29 -0600

    Se corrigue html y se añade arrays para las propiedades

---
+++ assets/js/data_propiedades.js
@@ -0,0 +1,126 @@
+const data_propiedades = [
+    {
+    idPropiedad: 1,    
+    nombreTituloCard: 'Apartamento 1',
+    src: '',
+    descripcion: 'Este apartamento de luji esta ubicado ....',
+    ubicacion: '123, luxury lane',
+    habitaciones: 0,
+    baños: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    },
+    {
+        idPropiedad: 2,    
+        nombreTituloCard: 'Apartamento 2',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 3,    
+        nombreTituloCard: 'Apartamento 3',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 4,    
+        nombreTituloCard: 'Apartamento 4',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 5,    
+        nombreTituloCard: 'Apartamento 5',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 6,    
+        nombreTituloCard: 'Apartamento 6',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 7,    
+        nombreTituloCard: 'Apartamento 7',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 8,    
+        nombreTituloCard: 'Apartamento 8',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 9,    
+        nombreTituloCard: 'Apartamento 9',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+    {
+        idPropiedad: 10,    
+        nombreTituloCard: 'Apartamento 10',
+        src: '',
+        descripcion: 'Este apartamento de luji esta ubicado ....',
+        ubicacion: '123, luxury lane',
+        habitaciones: 0,
+        baños: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    }
+
+    ]
+    
+
+
